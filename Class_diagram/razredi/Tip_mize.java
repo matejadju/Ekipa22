43f07e12b620668a20c5f71a98fe53d7027f2db3
Mateja <mateja.djuric@student.um.si>
2023-12-28 12:58:35 +0100

    class diagram

---
+++ Class_diagram/razredi/Tip_mize.java
@@ -0,0 +1,51 @@
+package si.um.feri.ris.models;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+public class Tip_mize {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private int steiloOseb;
+    private String naziv;
+    private String opis;
+
+    @OneToMany(mappedBy = "tip_mize_miza", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    Collection<Miza> tip_miza;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getSteiloOseb() {
+        return steiloOseb;
+    }
+
+    public void setSteiloOseb(int steiloOseb) {
+        this.steiloOseb = steiloOseb;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+}
