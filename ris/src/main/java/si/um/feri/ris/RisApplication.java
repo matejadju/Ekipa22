3413478622f50e5aab199519f84f4820b3e5ed54
Lazar Cvorovic <lazar.cvorovic@student.um.si>
2024-02-06 20:01:55 +0100

    klub

--- ris/src/main/java/si/um/feri/ris/RisApplication.java
+++ ris/src/main/java/si/um/feri/ris/RisApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RisApplication {
+public class
+RisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RisApplication.class, args);
