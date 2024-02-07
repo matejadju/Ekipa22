2a2e4f2cd84810aea25dae39e049d81fb0b271d1
unknown <stefan.vujicic@student.um.si>
2024-02-07 17:18:04 +0100

    Sprint1aMiza in Dogodek

--- ris/src/main/java/si/um/feri/ris/repository/MizaRepository.java
+++ ris/src/main/java/si/um/feri/ris/repository/MizaRepository.java
@@ -9,11 +9,17 @@ import si.um.feri.ris.models.Miza;
 import si.um.feri.ris.models.Uporabnik;
 import si.um.feri.ris.models.Vrsta;
 
-
-    public interface MizaRepository extends JpaRepository<Miza, Long> {
-
+import java.util.List;
 
 
+public interface MizaRepository extends JpaRepository<Miza, Long> {
 
-    }
+
+
+
+    @Query("SELECT m FROM Miza m WHERE m.status = :status AND m.tip_mize_miza.id = :tip_mize_miza_id")
+    List<Miza> findByStatusAndTipMizeId(boolean status, Long tip_mize_miza_id);
+
+
+}
 
