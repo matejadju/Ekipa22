dd63992b54ea41e130f6cda248ee0f1f55e6242d
W10 <mateja.djuric@student.um.si>
2025-10-21 14:12:12 +0200

    update

--- ris/src/test/java/si/um/feri/ris/MizaControllerTest.java
+++ ris/src/test/java/si/um/feri/ris/MizaControllerTest.java
@@ -15,7 +15,7 @@ import si.um.feri.ris.service.MizaService;
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, properties = "spring.profiles.active=ci")
 @Transactional
 public class MizaControllerTest {
 
